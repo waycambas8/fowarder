b858dac6a01dc0c4e3c94984a223b17b15016e3a
waycambas8 <waycambas8@gmail.com>
2021-12-14 20:02:46 +0700

    updated fowarde for all

--- app/Http/Controllers/FowarderController.php
+++ app/Http/Controllers/FowarderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class FowarderController extends Controller
 {   
@@ -13,6 +14,22 @@ class FowarderController extends Controller
     }
 
     public function callback_fowarder(Request $req){
+        if(isset($req['external_id'])){
+            $external_id = explode("-",$req['external_id']); 
+        }elseif(isset($req['data']['reference_id'])){
+            $external_id = explode("-",$req['data']['reference_id']); 
+        }else{
+            $external_id = explode("-",$req['qr_code']['external_id']);
+        }
+        
+        $pc = (isset($external_id[4]))?$external_id[4]:"";
+        
+        if(!Member::where("pc",$pc)->exists()){
+            $res['pesan'] = "PC Tidak terdaftar";
+            return $res;
+        }
+        
+        $member = Member::where("pc",$pc)->first();
         
         if(isset($this->header['X-Callback-Token'])){
             $token = $this->header['X-Callback-Token'];
@@ -25,7 +42,7 @@ class FowarderController extends Controller
         $data = $req->toArray();
         $curl = curl_init();
         curl_setopt_array($curl, array(
-          CURLOPT_URL => $this->url,
+          CURLOPT_URL => $member->url_callback,
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => '',
           CURLOPT_MAXREDIRS => 10,
